8d0565170edc94066365e69f8052670891f9716d
KNUPZAO <nicolas.resende@escola.pr.gov.br>
2025-08-25 09:40:20 -0300

    ordenei as listas

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
  # Lista de convidados que foram convidados para a festa
+convidados_da_festa = ['nayelli','igor','gabriel','bianca', 'daniela', 'brenno']
+
+# dicionario vazio para armazenar o status de presença de cada pessoa
+status_presenca = {}
+
+#esta e a lista de pessoas que tentaram entrar na festa
+lista_de_chegadas = ['igor','gabriel,' 'bianca', 'daniela']
